6737d14d862cd955cff3ee32f2eaacda65531152
Gaëtan Trellu <gaetan.trellu@gmail.com>
2023-03-04 21:31:25 -0500

    [database] Support SHORTGIC_DB_PATH env variable

--- app/database.py
+++ app/database.py
@@ -1,15 +1,18 @@
 """SQLAlchemy database URL and engine
 """
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Path to the SQLite3 database
-SQLALCHEMY_DATABASE_URL = "sqlite:///./shortgic.db"
+database_path = os.getenv("SHORTGIC_DB_PATH", "./shortgic.db")
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{database_path}"
 
 engine = create_engine(
     # Required with SQLite3 because it's not multi-threaded
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
